8d41e20668e8f6083387aefe0921be0349e4994d
Jongyoon Lee <jongyoon156@gmail.com>
2025-06-19 14:45:26 +0900

    Initial commit of Xflix project

---
+++ dddd.py
@@ -0,0 +1,13 @@
+import surprise
+surprise.__version__
+
+
+import numpy
+print(numpy.__version__)
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+print(np.__version__)
+print(pd.__version__)
+print()
